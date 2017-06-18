bde507111fe88f8facdbc455063f42dde38e2ca8
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-06-18 15:42:42 -0400

    Make the program generate forever. Also maybe some random rule stuff

--- cellular-automata.swift
+++ cellular-automata.swift
@@ -1,11 +1,6 @@
-let iters = 128
-let rule = 54
+import Foundation
 
-// I use a dictionary here because most rules result in not all the
-// spaces being filled. This depends on the exact rule, but it's likely that
-// less that half will be filled given a random row and random rule, 
-// so it's probably ok do this. Also this makes centering the row around zero much easier
-var row = [Int: Bool]()
+let iters = 127
 
 func in_rule(rule: Int, left: Bool, center: Bool, right: Bool) -> Bool {
     switch (left, center, right) {
@@ -35,27 +30,36 @@ func right_pad(_ string: String, _ num_spaces: Int) -> String {
     let count = (num_spaces - length) / 2
     return String(repeating: " ", count: count) + string
 }
+while true {
+    // let rule = Int(arc4random_uniform(256))
+    let rule = Int(readLine()!)!
+    // I use a dictionary here because most rules result in not all the
+    // spaces being filled. This depends on the exact rule, but it's likely that
+    // less that half will be filled given a random row and random rule, 
+    // so it's probably ok do this. Also this makes centering the row around zero much easier
+    var row = [Int: Bool]()
 
-row[0] = true
-print(right_pad("█", 1 + (iters * 2)))
-for i in 0..<iters {
-    var next_row = [Int: Bool]()
-    var row_string = ""
-    // must do plus one because we want to iterate one more cell than last time
-    for j in -(i + 1)...(i + 1) {
-        let left = row.get(j - 1, defaultValue: false)
-        let center = row.get(j, defaultValue: false)
-        let right = row.get(j + 1, defaultValue: false)
+    row[0] = true
+    print(right_pad("█", 1 + (iters * 2)))
+    for i in 0..<iters {
+        var next_row = [Int: Bool]()
+        var row_string = ""
+        // must do plus one because we want to iterate one more cell than last time
+        for j in -(i + 1)...(i + 1) {
+            let left = row.get(j - 1, defaultValue: false)
+            let center = row.get(j, defaultValue: false)
+            let right = row.get(j + 1, defaultValue: false)
 
-        // rule 30: (left && !(center || right)) || (!left && (center || right))
-        // rule 90: (left != right) && !center
-        if (in_rule(rule: rule, left: left, center: center, right: right)) {
-            next_row[j] = true
-            row_string += "█"
-        } else {
-            row_string += " "
+            // rule 30: (left && !(center || right)) || (!left && (center || right))
+            // rule 90: (left != right) && !center
+            if (in_rule(rule: rule, left: left, center: center, right: right)) {
+                next_row[j] = true
+                row_string += "█"
+            } else {
+                row_string += " "
+            }
         }
+        row = next_row
+        print(right_pad(row_string, 1 + (iters * 2)))
     }
-    row = next_row
-    print(right_pad(row_string, 1 + (iters * 2)))
 }
